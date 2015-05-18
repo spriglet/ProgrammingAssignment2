8cbee770ce725fe8ace46d1fe5644438cc4c9d61
Spriglet <redfroglet@gmail.com>
2015-05-17 20:51:55 -0600

    my coursera week2 R programming Project

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##  This function takes a matrix as paramater or creates an empty matrix and an inverse variable. 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL  # inverse variable
+    set <- function(y){ # set allows you to reset the x matrix variable. 
+      x <<- y   
+      inverse <<- NULL
+    }
+    get <- function() x #get returns the amtrix
+    setinverse <- function(inv) inverse <<- inv  # set inverse sets the inverse matrix
+    getinverse <- function() inverse  # get inverse returns the inverse 
+    list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Returns a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()   # gets the inverse of x
+    if(!is.null(inverse)) {  #if the value is not NULL thne it returns the inverse
+      message("getting cached data")
+      return(inverse)
+    }
+    data <- x$get()  # gets the x matrix
+    inverse  <- solve(data)  # sets the inverse
+    x$setinverse(inverse)  # caches in the inverse vaialbe of the x matrix. 
+    inverse 
 }
